c23d926a8a8da7d4ad4ecc2259f9eb89c385255c
Alison Hedigliranes <alison.hedigliranes@gmail.com>
2018-04-24 01:59:19 -0300

    update

--- src/grid.cpp
+++ src/grid.cpp
@@ -5,9 +5,12 @@ namespace lg{
 	Grid::Grid (dimension_type r, dimension_type c) : 
 				n_rows(r), n_columns(c) {
 
+		//alocar a grid
 		grid = new cell_type*[r];
 		for (int i = 0; i < r; i++){
 			grid[i] = new cell_type[c];
+
+			// inicializar os valores das células com status DEAD
 			for (int j = 0; j < c; j++){
 				this->at(i,j).set_status(status_type::DEAD);
 			}
@@ -16,12 +19,14 @@ namespace lg{
 	}
 
 	Grid::~Grid (){
+		// desalocar a grid
 		for (int i = 0; i < n_rows; i++)
     		delete [] grid[i];
 		delete [] grid;
 	}
 
 	void Grid::reset_neighbors(){
+		// para todas as células da grid, calcule os vizinhos
 		for (int i = 0; i < n_rows; i++){
 			for (int j = 0; j < n_columns; j++){
 				this->at(i,j).set_n_neighbors(0);
@@ -30,8 +35,11 @@ namespace lg{
 	}
 
 	void Grid::calculate_neighbors(){
+		// percorre as células da grid
 		for (int i = 0; i < n_rows; i++){
 			for (int j = 0; j < n_columns; j++){
+
+				//se a célula for viva
 				if (this->at(i,j).get_status() == status_type::ALIVE){
 
 					// analisa visinhança
@@ -53,12 +61,18 @@ namespace lg{
 	}
 
 	void Grid::next_gen(){
+		// para todas as células na grid
 		for (int i = 0; i < n_rows; i++){
 			for (int j = 0; j < n_columns; j++){
+				// se a célula for viva e possuir menos que 1 ou mais que
+				// 4 vizinhos, então ela morrerá na próxima geração
 				if ((this->at(i,j).get_status() == status_type::ALIVE) and
 				   (this->at(i,j).get_n_neighbors() <= 1 or 
 					this->at(i,j).get_n_neighbors() >= 4))
 					this->at(i,j).set_status(status_type::DEAD);
+
+				// se a célula for morta, mas possuir 3 vizinhos, ela irá
+				// nascer na próxima geração
 				else if (this->at(i,j).get_status() == status_type::DEAD and
 				 		 this->at(i,j).get_n_neighbors() == 3)
 					this->at(i,j).set_status(status_type::ALIVE);	
@@ -71,14 +85,22 @@ namespace lg{
 									  index_type i_min, 
 								      index_type i_max){
 
-		if (v < i_min) 		 return i_max - (i_min - v);
+		// se o índice é menor que o menor índice, 
+		// então retorne a diferença como offset pro índice máximo
+		if (v < i_min) return i_max - (i_min - v);
+
+		// análogo à condição anterior, porém o offset é calculado
+		// em cima do menor índice com base no maior índice
 		else if (v >= i_max) return i_min + (v - i_max);
-		else  			     return v;
+
+		// se o índice estiver nos limites, então não há o que fazer
+		else return v;
 
 	}
 
 	Grid::cell_type& Grid::at(index_type x, 
 							  index_type y){
+		// retorna a posição grid[x][y] levando em consideração o wrap
 		return this->grid[this->wrap_index(x, 0, n_rows)]
 						 [this->wrap_index(y, 0, n_columns)];
 
