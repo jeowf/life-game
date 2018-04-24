c728f91dbe056496ba1857203d68ece704914c3d
Felipe Morais <moraisfelipe97@gmail.com>
2018-04-24 19:48:32 -0300

    Gen e Cell documentadas

--- include/cell.h
+++ include/cell.h
@@ -3,30 +3,84 @@
 
 namespace lg{
 
+	/*! @class Cell
+	 *
+     * @brief A class that storages and manages cells status
+	*/
+	
 	class Cell{
-		// Alias
+
+		///
+		/// Alias
+		///
+
 	    public:
-		typedef int neighbors_type;
-		typedef int& ref; 
+		typedef int neighbors_type; /*!< Type of neighbors */
+		typedef int& ref; /*!< Reference cell*/
+
+		/*! @enum Cell
+		 *
+	     * @brief Enum cells Types (DEAD OR ALIVE).
+		*/
 
 		enum class status_type : short int { DEAD = 0, ALIVE  };
 		
-		// Atributos
+		///
+		/// Attributes
+		///
+
 		private:
-		status_type status;
-		neighbors_type n_neighbors; 
+		status_type status; /*!< Type of status*/
+		neighbors_type n_neighbors; /*!< Number of neighbors*/
 		
-		// Métodos
+		
+		///
+		/// Methods
+		///
+
 		public:
-		// Construtores
+		/*! 
+		 * @brief Cell Constructor
+	 	 *
+		 * Initialize this Cell with DEAD or ALIVE and number of Neighbors
+		 *
+		 * @param r status
+		 * @param c number of neighbors
+		*/
 		Cell (status_type, neighbors_type);
+
+		/*! 
+		 * @brief Cell Destructor
+		 *
+		 * Destroys this Cell
+		*/
 		Cell ();
 
-		// Acessores
+		/*! 
+		 * @brief Get Status
+		 *
+		 * @return status 
+		*/
 		status_type get_status( void ) const;
+		/*! 
+		 * @brief Set Status
+		 *
+		 * @param s DEAD or ALIVE 
+		*/
 		void set_status( status_type );
 
+		/*! 
+		 * @brief Get Number of Neighbors
+		 *
+		 * @return number of neighbors 
+		*/
 		neighbors_type get_n_neighbors( void ) const;
+
+		/*! 
+		 * @brief Set Number of Neighbors
+		 *
+		 * @param n number of neighbors ALIVE
+		*/
 		void set_n_neighbors( neighbors_type );
 
 	};
--- include/gen.h
+++ include/gen.h
@@ -5,35 +5,95 @@
 
 namespace lg{
 
+	/*! @class Generations
+	 *
+     * @brief A class that storages and manages generations status
+	*/	
+
 	class Gen{
-		// Alias
+		///
+		/// Alias
+		///
 	    public:
-	    typedef int index_type;
-	    typedef int size_type;
+	    typedef int index_type; /*!< Type of index */
+	    typedef int size_type; /*!< Type of size */
 
 		private:
+		/*! @class Coordinates
+	 	*
+    	* @brief A class that represents a coordinate
+		*/	
 		class Coord{
 			public:
-			index_type x;
-			index_type y;
-			
+			index_type x; /*!< x of coordinate */
+			index_type y; /*!< y of coordinate */
+			 /*! 
+			 * @brief Coordinate Constructor
+		 	 *
+			 * Set a coordinate of ALIVE cell
+			 *
+			 * @param r status
+			 * @param c number of neighbors
+			*/
 			Coord(index_type, index_type);
-
+			/*! 
+			 * @brief '==' Overload
+		 	 *
+			 * Overload '==' operator to verify if a coordinate is equal
+			 *
+			 * @param rhs coordinate of a cell
+			 *
+			 * @return true if this coordinate is equal
+			*/
 			bool operator==(const Coord&) const;
 		};
 
-		std::vector<Coord> gen;
+		std::vector<Coord> gen; /*!< Vector of coordinates */
 
-		// Métodos
+		
+		///
+		/// Methods
+		///
+
 		public:
-		// Construtores
-
+		/*! 
+		 * @brief Add
+		 *
+		 * Add a coordinate in a vector 
+		 *
+		 * @param i coordinate x
+		 * @param j coordinate j
+		 *
+		*/	
 		void add(index_type i, index_type j);
-
+		/*! 
+		 * @brief Size
+		 *
+		 * Return the size of vector os coordinates 
+		 *
+		 * @return size of vector os coordinates 
+		 *
+		*/	
 		size_type size() const;
-
+			/*! 
+			 * @brief '==' Overload
+		 	 *
+			 * Overload '==' operator to verify if a generation is stable
+			 *
+			 * @param rhs type generation
+			 *
+			 * @return true if this generation is equal rhs
+			*/
 		bool operator==(const Gen&) const;
-
+			/*! 
+			 * @brief '[]' Overload
+		 	 *
+			 * Overload '[]' operator to take a element in vector of coordinates
+			 *
+			 * @param i index of you want
+			 *
+			 * @return the element in index of vector
+			*/
 		const Coord& operator[](index_type) const;
 
 	};
